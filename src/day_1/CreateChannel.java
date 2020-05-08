26215716574de4c244ffc1caf9a8d0b2960f81c2
Svitlana Fedorova <63323272+feku1228@users.noreply.github.com>
2020-05-08 15:20:47 -0400

    Update CreateChannel.java

--- src/day_1/CreateChannel.java
+++ src/day_1/CreateChannel.java
@@ -3,5 +3,8 @@ package day_1;
 public class CreateChannel {
     public static void main(String[] args) {
         System.out.println("Hello Svitlana");
+        
+                System.out.println("Hello Svitlana, i love you)");
+
     }
 }
