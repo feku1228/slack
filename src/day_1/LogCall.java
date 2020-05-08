0e61c796294d19f2c71265a4eee2d97dcdd75273
feku1228 <feku1228@gmail.com>
2020-05-08 15:38:36 -0400

    add

--- src/day_1/LogCall.java
+++ src/day_1/LogCall.java
@@ -1,7 +1,10 @@
 package day_1;
 
 public class LogCall {
+    public static void main(String[] args) {
+
+
     //user are able to call through slack
             System.out.println("Hello Svitlana");
 
-}
+}}
